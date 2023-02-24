dc0b49d13002cc65f50d900223fcc92481b93c96
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-24 18:04:58 +0100

    Print cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,22 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Ciao</h1>
+    <div class="container">
+        <div class="row justify-content-between">
+        @foreach ($movies as $movie)
+      <div class="col-3 mx-2">
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{$movie['title']}}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{$movie['original_title']}}</h6>
+          <p class="card-text">{{$movie['nationality']}}</p>
+          <span href="#" class="card-link">{{$movie['date']}}</span>
+          <span href="#" class="card-link">{{$movie['vote']}}</span>
+        </div>
+      </div>
+      </div>
+      @endforeach
+        </div>
+    </div>
 </body>
 </html>
